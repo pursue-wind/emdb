d6a3b3b2f8a173ca2138ce64c073a3b407adc585
harry1328553053 <a1328553053@gmail.com>
2023-11-01 14:56:55 +0800

    add additional_info infomation

--- db/pgsql/tv_series_additional.py
+++ db/pgsql/tv_series_additional.py
@@ -8,7 +8,6 @@ from sqlalchemy.dialects.postgresql import insert
 @gen.coroutine
 @exc_handler
 def insert_tv_additional_info(tv_additional_info, **kwargs):
-
     sess = kwargs.get('sess')
     tv_additional_info = {k: v for k, v in tv_additional_info.items() if v is not None}
     stmt = insert(TVSeriesAdditional).values(tv_additional_info).on_conflict_do_nothing()
@@ -18,3 +17,12 @@ def insert_tv_additional_info(tv_additional_info, **kwargs):
     sess.commit()
 
     return dict()
+
+
+@gen.coroutine
+@exc_handler
+def get_tv_additional_info(series_id, **kwargs):
+    sess = kwargs.get('sess')
+    result = sess.query(TVSeriesAdditional).filter(TVSeriesAdditional.tmdb_series_id == series_id).first()
+
+    return dict(data=result.to_dict())
--- handlers/tv_handler.py
+++ handlers/tv_handler.py
@@ -1,6 +1,7 @@
 from db.pgsql.movies import query_movie_by_company_id, query_movie_by_name, query_movie_by_tmdb_id
 from db.pgsql.tv_episodes import get_tv_episodes_list
 from db.pgsql.tv_seasons import get_tv_season_params
+from db.pgsql.tv_series_additional import get_tv_additional_info
 from handlers.base_handler import BaseHandler
 from tornado import gen
 from db.pgsql.enums.enums import get_key_by_value, GenresType
@@ -120,12 +121,11 @@ class SearchCompanyTV(BaseHandler):
         page_size = int(args.page_size)
         if not all([tmdb_company_id]):
             self.fail(402)
-        print(args)
         yield self.check_auth()
         result = yield query_movie_by_company_id(tmdb_company_id, SourceType.Tv.value, movie_name=args.tv_name,
                                                  page_num=page_num,
                                                  page_size=page_size)
-        print(result)
+
         if "status" in result and result["status"] != 0:
             self.fail(1)
         data = result.get('data', None)
@@ -154,6 +154,22 @@ class SearchCompanyTV(BaseHandler):
             tv['episode_count'] = season_name['data']['episode_count']
             tv['season_external_ids'] = season_name['data']['external_ids']
             tv['season_number'] = season_name['data']['season_number']
+            result = yield get_tv_additional_info(tv['tmdb_series_id'])
+
+            additional_info = result.get('data')['data']
+            if additional_info['first_air_date'] is not None:
+                first_air_date = additional_info['first_air_date'].strftime('%Y-%m-%d %H:%M:%S')
+            else:
+                first_air_date = None
+            additional_info['first_air_date'] = first_air_date
+
+            if additional_info['last_air_date'] is not None:
+                last_air_date = additional_info['last_air_date'].strftime('%Y-%m-%d %H:%M:%S')
+            else:
+                last_air_date = None
+            additional_info['last_air_date'] = last_air_date
+            tv['additional_info']=additional_info
+
             tvs.append(tv)
         self.success(data=dict(page_num=page_num,
                                page_size=page_size,
