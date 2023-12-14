5eaf9c29fa231f8f6c18ca916ad29a9588eff705
harry1328553053 <a1328553053@gmail.com>
2023-12-14 18:13:59 +0800

    update ID list

--- import_tv_to_emdb_by_series_id.py
+++ import_tv_to_emdb_by_series_id.py
@@ -3,8 +3,8 @@ from tornado import ioloop
 from service.fetch_tv_series_info import get_tv_detail, import_tv_emdb_by_series_id
 
 
-tmdb_series_id_list = [122790, 208066, 130955, 131714, 124069, 112356, 111028, 109872, 240220]
-company_id = 100000001
+tmdb_series_id_list = [232373, 126392, 64777, 130519, 237318, 91755, 156951, 204979, 205110]
+company_id = 100000000
 if __name__ == '__main__':
     io_loop = ioloop.IOLoop.current()
     # io_loop.run_sync(lambda: get_tv_detail(122790, None))
