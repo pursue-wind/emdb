03c69b3d2779585e63c9fa058fa4ddcd2371ca20
van <18666205983@163.com>
2024-01-10 16:53:04 +0800

    update tv episode

--- db/pgsql/tv_episodes.py
+++ db/pgsql/tv_episodes.py
@@ -30,8 +30,7 @@ def insert_tv_episodes_list(tv_episodes_list, **kwargs):
 @exc_handler
 def get_tv_episodes_list(tmdb_season_id, **kwargs):
     sess = kwargs.get('sess')
-    result = (sess.query(TVEpisodes).filter(TVEpisodes.tmdb_season_id == tmdb_season_id).
-              order_by(TVEpisodes.episode_number))
+    result = sess.query(TVEpisodes).filter(TVEpisodes.tmdb_season_id == tmdb_season_id).order_by(TVEpisodes.episode_number.asc())
     total = result.count()
     episodes_list = result.all()
     episodes=[]
