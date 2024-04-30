31d9df663ac2ac469155c78182c560dff7caa12e
hhq365 <hhq365@163.com>
2024-04-30 16:38:26 +0800

    update set tmdb_id+source_type as unique instead tmdb_id

--- docs/tmdb/update_tmdb_index.sql
+++ docs/tmdb/update_tmdb_index.sql
@@ -3,6 +3,7 @@ CREATE UNIQUE INDEX ix_uni_movies_tmdb_id_source_type ON movies (tmdb_id,source_
 
 
 ALTER TABLE movie_key_words ADD COLUMN source_type int not null default 1;
+delete from movie_key_words mk where not exists(select * from movies where id=mk.movie_id);
 update movie_key_words mk set source_type=(select source_type from movies where id=mk.movie_id);
 
 
